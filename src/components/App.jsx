1006efba3615d62a0976880e4dfa24613b7e47d4
James Conlin <jacon.dev@outlook.com>
2025-04-02 11:33:35 +0100

    creating core concepts component and changing UI to add new component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,19 @@
 import Header from './Header';
 import MainGoal from './MainGoal';
+import CoreConcepts from "./CoreConcepts";
+import componentsImage from '../assets/components.png';
 
 function App() {
   return (
     <div>
         <Header />
       <main>
-        <h2>Time to get started!</h2>
-          <MainGoal />
+          <section id="core-concepts">
+              <h2>Core Concepts</h2>
+              <ul>
+                <CoreConcepts title="Components" description="The core UI building block." image={componentsImage} />
+              </ul>
+          </section>
       </main>
     </div>
   );
