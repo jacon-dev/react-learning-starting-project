dd1d53e912ad91e08b205444b611b17b2a1cdc2b
James Conlin <jacon.dev@outlook.com>
2025-04-02 11:38:34 +0100

    refactoring code to use simpler implementations to get core concept data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,10 @@ function App() {
           <section id="core-concepts">
               <h2>Core Concepts</h2>
               <ul>
-                <CoreConcepts title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image} />
+                  <CoreConcepts {...CORE_CONCEPTS[0]} />
+                  <CoreConcepts {...CORE_CONCEPTS[1]} />
+                  <CoreConcepts {...CORE_CONCEPTS[2]} />
+                  <CoreConcepts {...CORE_CONCEPTS[3]} />
               </ul>
           </section>
       </main>
