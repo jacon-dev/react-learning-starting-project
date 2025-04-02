1bdf35c3bbb775986f2b1d81eb76bbb964e457c6
James Conlin <jacon.dev@outlook.com>
2025-04-02 10:42:50 +0100

    Added new MainGoal component and refactored out individual components

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import Header from './Header';
+import MainGoal from './MainGoal';
+
+function App() {
+  return (
+    <div>
+        <Header />
+      <main>
+        <h2>Time to get started!</h2>
+          <MainGoal />
+      </main>
+    </div>
+  );
+}
+
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
     return (
         <header>
-            <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+            <img src="../assets/react-core-concepts.png" alt="Stylized atom" />
             <h1>React Essentials</h1>
             <p>
                 Fundamental React concepts you will need for almost any app you are
@@ -11,15 +11,4 @@ function Header() {
     );
 }
 
-function App() {
-  return (
-    <div>
-        <Header />
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
-}
-
-export default App;
+export default Header;
