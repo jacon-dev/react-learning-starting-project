26c5ffc0daf591591164e2f7e179e8f1388fd281
James Conlin <jacon.dev@outlook.com>
2025-04-02 10:45:15 +0100

    Fixing broken image source

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
     return (
         <header>
-            <img src="../assets/react-core-concepts.png" alt="Stylized atom" />
+            <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
             <h1>React Essentials</h1>
             <p>
                 Fundamental React concepts you will need for almost any app you are
