97be74c0b2edb237b1f613b2471e2bb8d96bcdd4
James Conlin <jacon.dev@outlook.com>
2025-04-03 09:00:25 +0100

    updating datajs and adding new functionality to load conditional content based on button clicks

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,24 @@ import TabButton from "./components/TabButton/TabButton";
 import {useState} from 'react'
 
 import {CORE_CONCEPTS} from "./data";
+import {EXAMPLES} from "./data";
 
 function App() {
-    const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+    const [selectedTopic, setSelectedTopic] = useState();
+
+    let tabContent = <p>Please select a topic.</p>;
+    if(selectedTopic)
+        tabContent = (
+            <div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                    <code>
+                        {EXAMPLES[selectedTopic].code}
+                    </code>
+                </pre>
+            </div>
+        )
 
     function handleSelect(selectedButton) {
         setSelectedTopic(selectedButton);
@@ -28,12 +43,12 @@ function App() {
           <section id="examples">
               <h2>Examples</h2>
               <menu>
-                  <TabButton onSelect={() => handleSelect('components')}>Component</TabButton>
+                  <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
                   <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
                   <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
                   <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
               </menu>
-              {selectedTopic}
+              {tabContent}
           </section>
       </main>
     </div>
